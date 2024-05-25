f8881ce8b17694f3f79c7980ae60a9a4019c3204
Sanlato Americo <sanlato.americo@saferlimitada.com>
2024-05-25 16:44:28 +0200

    Refactor UserService

--- src/main/java/com/sanlato/demo/services/UserService.java
+++ src/main/java/com/sanlato/demo/services/UserService.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.sanlato.demo.models.user;
-import com.sanlato.demo.repositories.TaskRepository;
 import com.sanlato.demo.repositories.UserRepository;
 
 
@@ -18,30 +17,30 @@ public class UserService {
  @Autowired   
  private UserRepository userRepository;
 
- @Autowired
- private TaskRepository taskRepository;
+
 
 
  public user findById(Long id) {
     Optional<user> user =this.userRepository.findById(id);
     return user.orElseThrow(() -> new RuntimeException("usuario nao encontrado! id " + id + ", Tipo" + user.class.getName() ));
 }
+
  
  @Transactional
  public user create ( user obj) {
     obj.setId( null);
 obj = this.userRepository.save(obj);
-this.taskRepository.saveAll(obj.getTasks());
 return obj;
  }
+
 
 @Transactional
  public user update(user obj){
 user newObj = findById(obj.getId());
 newObj.setPassword(obj.getPassword());
 return  this.userRepository.save(newObj);
+}
 
- }
 
  public void delete (Long id){
     findById(id);
