276e05710533a6e88045807230f5353bcc78544b
Sanlato Americo <sanlato.americo@saferlimitada.com>
2024-06-06 22:30:36 +0200

    Create TaskController

--- src/main/java/com/sanlato/demo/services/TaskService.java
+++ src/main/java/com/sanlato/demo/services/TaskService.java
@@ -1,5 +1,6 @@
 package com.sanlato.demo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +27,12 @@ public Task findById(long id){
         "Tarefa nao encontrada! id:" + id + ", Tipo: " + Task.class.getName()));
    
 }
+
+public List<Task> findAllByUserId(Long userid){
+    List<Task> tasks = this.taskRepository.findByUser_Id(userid);
+    return tasks;
+} 
+
 
 @Transactional
 public Task create (Task obj){
